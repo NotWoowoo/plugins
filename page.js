b610bf8ea0abde5bb8e5092585abc6a1d65d155b
U-LAPTOP-GAU0OU56\gamer <gamerwoowoo6@gmail.com>
2020-11-16 00:34:39 -0600

    made it so transitions start only after load

--- page.js
+++ page.js
@@ -2,3 +2,7 @@ document.addEventListener("DOMContentLoaded", main);
 function main(){
     document.title = "Woowoo's plugins";
 }
+
+$(window).load(function() {
+    $("body").removeClass("preload");
+  });
