62f6209818cc961469702b1824f216eb4ace5a04
U-LAPTOP-GAU0OU56\gamer <gamerwoowoo6@gmail.com>
2020-11-16 01:23:56 -0600

    actually made it so that css animations only start when tab opened

--- page.js
+++ page.js
@@ -1,8 +1,19 @@
+document.body.classList.add('js-loading');
+window.addEventListener("load", function(){
+    if(!document.hidden){
+        document.body.classList.remove('js-loading');
+    }
+});
+
+document.addEventListener('visibilitychange', function(e) {
+    document.body.classList.remove('js-loading');
+});
+
+document.onload = function(){
+    console.log("fff");
+}
+
 document.addEventListener("DOMContentLoaded", main);
 function main(){
     document.title = "Woowoo's plugins";
 }
-
-$(window).load(function() {
-    $("body").removeClass("preload");
-  });
